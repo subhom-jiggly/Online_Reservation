97e5c4954508a455964da4c5522c054078491908
Subhom Mazumdar <subhom2003@gmail.com>
2023-06-24 19:34:08 +0530

    commit

---
+++ src/online_reservation/Online_Reservation.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package online_reservation;
+
+/**
+ *
+ * @author subho
+ */
+public class Online_Reservation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new StartPage().setVisible(true);
+    }
+    
+}
